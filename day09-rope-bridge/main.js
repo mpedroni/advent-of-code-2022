9392ddecc890a85ecf435edc934a5db5f5d8fe2d
Matheus Pedroni <mpedroni77@gmail.com>
2023-02-05 19:20:32 -0300

    day nine, part two

--- day09-rope-bridge/main.js
+++ day09-rope-bridge/main.js
@@ -11,41 +11,53 @@ const motions = input
 
 // [x, y]
 const head = [0, 0];
-const tail = [0, 0];
+const knobs = [];
 
-const positions = new Set();
+for (let knob = 0; knob < 10; knob++) {
+  knobs.push([0, 0]);
+}
+
+const positions = [];
+
+for (let knob = 0; knob < 10; knob++) {
+  positions.push(new Set());
+}
 
 for (const motion of motions) {
   for (let i = 0; i < motion.count; i++) {
-    positions.add(tail.toString());
     move(head, motion.direction);
+    for (let knob = 0; knob < knobs.length; knob++) {
+      const tail = knobs[knob];
+      const h = knob == 0 ? head : knobs[knob - 1];
 
-    if (!isTailTouchingHead()) {
-      const [x, y] = getDistanceBetweenHeadAndTail();
+      positions[knob].add(tail.toString());
 
-      const hX = head[0];
-      const tX = tail[0];
-      const hY = head[1];
-      const tY = tail[1];
+      if (!isTailTouchingHead(tail, h)) {
+        const [x, y] = getDistanceBetweenHeadAndTail(tail, h);
 
-      // console.log(head, tail, [x, y]);
-      if ((x > 1 && y != 0) || (y > 1 && x != 0)) {
-        // diagonal movement
-        move(tail, hX > tX ? "R" : "L");
-        move(tail, hY > tY ? "U" : "D");
-      } else if (x > 1) {
-        move(tail, hX > tX ? "R" : "L");
-      } else if (y > 1) {
-        move(tail, hY > tY ? "U" : "D");
+        const hX = h[0];
+        const tX = tail[0];
+        const hY = h[1];
+        const tY = tail[1];
+
+        if ((x > 1 && y != 0) || (y > 1 && x != 0)) {
+          // diagonal movement
+          move(tail, hX > tX ? "R" : "L");
+          move(tail, hY > tY ? "U" : "D");
+        } else if (x > 1) {
+          move(tail, hX > tX ? "R" : "L");
+        } else if (y > 1) {
+          move(tail, hY > tY ? "U" : "D");
+        }
+
+        positions[knob].add(tail.toString());
       }
-
-      positions.add(tail.toString());
     }
   }
 }
 
-function isTailTouchingHead() {
-  const [x, y] = getDistanceBetweenHeadAndTail();
+function isTailTouchingHead(tail, head) {
+  const [x, y] = getDistanceBetweenHeadAndTail(tail, head);
 
   return x <= 1 && y <= 1;
 }
@@ -72,11 +84,12 @@ function move(el, direction) {
   }
 }
 
-function getDistanceBetweenHeadAndTail() {
+function getDistanceBetweenHeadAndTail(tail, head) {
   const x = Math.abs(head[0] - tail[0]);
   const y = Math.abs(head[1] - tail[1]);
 
   return [x, y];
 }
 
-console.log(head, tail, positions.size);
+console.log("part one", head, knobs[0], positions[0].size);
+console.log("part two", head, knobs[8], positions[8].size);
